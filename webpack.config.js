031d09cfe34ee1e61712145681285d11f4348fc9
CbaSage <cbasage@gmail.com>
2017-12-17 03:21:40 +0000

    Initial webpack + node dependency setup

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,8 @@ module.exports = {
                 loader: 'babel-loader',
                 exclude: /node_modules/,
                 query: {
-                    presets: ['es2015', 'react']
+                    presets: ['es2015', 'react', 'stage-2'],
+                    plugins: ['transform-class-properties']
                 }
             },
             {
@@ -48,7 +49,7 @@ module.exports = {
         ],
     },
     plugins: [
-        new CleanWebpackPlugin(['dist']),
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             title: 'Hot Module Replacement'
         }),
